e84a51b000c1392b3a897b18c701e9d5cf463730
Edgar Cuarezma <ecuarezma@gmail.com>
2020-06-06 16:19:00 -0500

    Refactor fetchPosts call in content component using useEffect dependency, so no more useState

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,27 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as actions from "../../store/actions";
 
 import BlogPost from "./BlogPost";
 
-const Content = (props) => {
-  const [state, setState] = useState(props.blog);
-
+const Content = ({ blog, fetchPosts }) => {
   useEffect(() => {
-    props.fetchPosts();
-  }, []);
+    fetchPosts();
+  }, [fetchPosts]);
 
-  useEffect(() => {
-    setState(props.blog);
-    console.log("From content component", props.blog, state);
-  }, [props.blog, state]);
-
-  if (!props.blog) {
+  if (!blog) {
     return <p>loading...</p>;
   }
   return (
     <div>
-      {props.blog.map((post) => (
+      {blog.map((post) => (
         <BlogPost {...post} key={post._id} />
       ))}
     </div>
