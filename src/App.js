41d512ee862e5b3f8d3f08e073b7607b6708127b
Edgar Cuarezma <ecuarezma@gmail.com>
2022-09-14 13:05:44 -0500

    clean up files, add horse animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import video from "./media/mirror.MOV";
+import horse from "./media/horse.MOV";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="title">
+        AEMI
+        <br />
+        KATO-CUAREZMA
+      </h1>
+      <div className="video">
+        <video playsInline autoPlay muted loop id="bgvid">
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
+      <video playsInline autoPlay muted loop id="horse-bg">
+        <source src={horse} type="video/mp4" />
+      </video>
     </div>
   );
 }
