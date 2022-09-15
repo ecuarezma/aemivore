6c1ef4f40e675acab37cfc4fa76d7050388ba616
Edgar Cuarezma <ecuarezma@gmail.com>
2022-09-15 12:12:45 -0500

    update mirror clip

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import "./App.css";
-import video from "./media/mirror.MOV";
+
 import horse from "./media/horse.MOV";
 
 function App() {
   return (
     <div className="App">
       <h1 id="title">
-        AEMI
-        <br />
-        KATO-CUAREZMA
+        AEMI K<span>A</span>TO
       </h1>
+
       <div className="video">
-        <video playsInline autoPlay muted loop id="bgvid">
+        <video playsInline autoPlay muted loop id="splash-vid">
           <source src={video} type="video/mp4" />
         </video>
       </div>
+
       <video playsInline autoPlay muted loop id="horse-bg">
         <source src={horse} type="video/mp4" />
       </video>
