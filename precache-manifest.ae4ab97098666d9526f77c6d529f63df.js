c1189cac948306e488996e802808ab2e15d76534
Edgar Cuarezma <ecuarezma@gmail.com>
2022-09-16 13:28:50 -0500

    Updates

---
+++ precache-manifest.ae4ab97098666d9526f77c6d529f63df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "914e77acf0fcbf7f6a24",
+    "url": "/aemivore/static/css/main.882689ba.chunk.css"
+  },
+  {
+    "revision": "914e77acf0fcbf7f6a24",
+    "url": "/aemivore/static/js/main.75f6c3ef.chunk.js"
+  },
+  {
+    "revision": "9f4a1ac294f36c1ee867",
+    "url": "/aemivore/static/js/runtime~main.9738069f.js"
+  },
+  {
+    "revision": "0efa9997be29d50db8a6",
+    "url": "/aemivore/static/js/2.d4d18f75.chunk.js"
+  },
+  {
+    "revision": "76def3fe25a1325124531edf760a3565",
+    "url": "/aemivore/static/media/mirror-hand.76def3fe.mov"
+  },
+  {
+    "revision": "fb58da91b8b9d96271e035f22e47889d",
+    "url": "/aemivore/static/media/horse.fb58da91.MOV"
+  },
+  {
+    "revision": "64e33a2523eaa17b9cf9a05f30e4ba8c",
+    "url": "/aemivore/index.html"
+  }
+];
