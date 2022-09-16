1fe0e695fa2aba6d2467873e462c13afe6338177
Edgar Cuarezma <ecuarezma@gmail.com>
2022-09-16 12:09:33 -0500

    add icons for email and instagram

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from "react";
 import "./App.css";
-import mirror from "./media/mirror-hand.mov";
+import video from "./media/mirror-hand.mov";
 import horse from "./media/horse.MOV";
+// import aki from "./media/horse/aki/aki_full.mov";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <h1 id="title">
+      <h1 id="header-name">
         AEMI K<span>A</span>TO
       </h1>
-
-      <div className="video">
-        <video playsInline autoPlay muted loop id="splash-vid">
-          <source src={mirror} type="video/mp4" />
-        </video>
-      </div>
-
+      <video playsInline autoPlay muted loop id="splash-vid">
+        <source src={video} type="video/mp4" />
+      </video>
+      <footer>
+        <div className="icon-email">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </div>
+        <div className="icon-insta">
+          <FontAwesomeIcon icon={faInstagram} />
+        </div>
+      </footer>
       <video playsInline autoPlay muted loop id="horse-bg">
         <source src={horse} type="video/mp4" />
       </video>
+      {/* <video playsInline autoPlay muted id="aki">
+        <source src={aki} type="video/mp4" />
+      </video> */}
     </div>
   );
 }
