14a741f54a77b284c7fb88ae4c3b8ff60a3315c4
Edgar Cuarezma <ecuarezma@gmail.com>
2022-10-13 13:39:40 -0500

    update .gitignore file

---
+++ build/precache-manifest.32be041e2563b3ee3db8e615af11d9c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "561920e4063c1443254c",
+    "url": "/static/css/main.9e67c4aa.chunk.css"
+  },
+  {
+    "revision": "561920e4063c1443254c",
+    "url": "/static/js/main.f06b2c5d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0efa9997be29d50db8a6",
+    "url": "/static/js/2.d4d18f75.chunk.js"
+  },
+  {
+    "revision": "cf2e2d5d417425628a1b2ccda9a13482",
+    "url": "/static/media/mirror_hand.cf2e2d5d.mp4"
+  },
+  {
+    "revision": "a15d4d83ea4040061442e2023b49e755",
+    "url": "/static/media/horse_running.a15d4d83.mp4"
+  },
+  {
+    "revision": "a232410c1d931e68661a7024d3d01eea",
+    "url": "/index.html"
+  }
+];
