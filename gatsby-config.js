5a6eb9e877fb5e6b043af17a8a0b401c2fdb3b54
Edgar Cuarezma <ecuarezma@gmail.com>
2022-10-31 10:15:58 -0500

    update config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,14 @@
 module.exports = {
   plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-fontawesome-css`,
+    `gatsby-plugin-sass`,
     {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "./src/assets/horse-icon.png",
       },
     },
-    `gatsby-plugin-fontawesome-css`,
   ],
 };
